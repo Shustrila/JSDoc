25122a5238917b6e0644f6e14ea011880b21ee78
Alexandr Maxalov <sanya.maxalov@gmail.com>
2019-02-08 22:09:00 +0300

    add

---
+++ src/js/character.js
@@ -0,0 +1,13 @@
+/**
+ * this class character
+ *
+ * @param {String} name - name
+ * @param {String} type - type
+ *
+ * @throws u made a mistake.
+ */
+
+function Character(name, type) {
+	this.name = name;
+	this.type = type;
+}
